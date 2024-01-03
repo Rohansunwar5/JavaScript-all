3ae7a2541fbf60f596373d4c48f0781d6f856903
Rohansunwar5 <80083391+Rohansunwar5@users.noreply.github.com>
2024-01-03 21:10:29 +0530

    update: operations done

--- Readme.md
+++ Readme.md
@@ -8,3 +8,13 @@ when a function or variable is declared and left without any input or data then,
 https://www.ecmascript.org/docs-dev/
 
 03 => We can change check the variable data type by using the 'typeof' function inside the console, for data type conversion of the variable, we use (Number, String) function within the variable [let valueInNumber = Number(score);]
+
+Note : in javaScript the executions follows line by line and across horizontly i.e why console.log(1+1+ "2") => 22 , since the first two are integers itself they get calculated at first, only after that calculted with string , another example is console.log("2" + 1 + 1) => 211 since the first value itself is the string the whole remaining value explicitly gets changed to string by js. 
+
+paraenthsis is important in using operations the right way 
+
+ for prefix and post fix => mdn.com
+
+while using prefix the value is already incremented and returned before it performs increment
+
+while using postfix the value increments and returns the value after incrementing
--- basics_1/03_conversionAndOperations.js
+++ basics_1/03_conversionAndOperations.js
@@ -20,6 +20,59 @@
 // ''=> false
 // 'Rohan' => true
 
-let someNumber = 33;
-let stringNumber = String(someNumber);
-console.log(typeof stringNumber);
+// let someNumber = 33;
+// let stringNumber = String(someNumber);
+// console.log(typeof stringNumber);
+
+// ************Operations**************
+
+// let value = 3;
+// let NegValue = -value; // -3
+// let dif = value + NegValue; // 0
+// console.log(NegValue);
+// console.log(value);
+// console.log(dif);
+
+// let str1 = "rohan";
+// let str2 = "hello";
+
+// let str3 = str1 + str2;
+// console.log(str3);
+
+// in javaScript the executions follows line by line and across horizontly i.e why console.log(1+1+ "2") => 22 , since the first two are integers itself they get calculated at first, only after that calculted with string , another example is console.log("2" + 1 + 1) => 211 since the first value itself is the string the whole remaining value explicitly gets changed to string by js.
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log("1" + (2 + 2)); //14
+
+// console.log(+true);
+// console.log(+"");
+
+// let num1, num2, num3;
+
+// num1 = num2 = num3 = 2 + 2;
+
+// let gameCounter = 100;
+// gameCounter++;
+// ++gameCounter;
+
+// for prefix and post fix => mdn.com
+
+// while using prefix the value is already incremented and returned before it performs increment
+
+// while using postfix the value increments and returns the value after incrementing
+
+// console.log(gameCounter);
+
+let x = 3;
+let y = x++;
+
+console.log(x);
+console.log(y);
+
+let a = 3;
+let b = ++a;
+
+console.log(a);
+console.log(b);
