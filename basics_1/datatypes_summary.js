48f39f6938de5e39417bfddb0a122bb11200b0dd
Rohansunwar5 <80083391+Rohansunwar5@users.noreply.github.com>
2024-01-04 21:52:47 +0530

    stack and heaps

--- basics_1/datatypes_summary.js
+++ basics_1/datatypes_summary.js
@@ -29,3 +29,28 @@ const myFunction = () => {
 };
 
 console.log(typeof myobj);
+
+//------------------------------------------------
+//stack (Primitive), heap(Non-primitive)
+
+// in stack the the variable only takes a copy of another datatype , so even if the value is changed from where the data is being refrenced , the original remains the same
+
+let myName = "Rohan";
+
+let anotherName = myName;
+anotherName = "sunwar";
+
+console.log(myName);
+console.log(anotherName);
+
+// in Non Primitive the variables points to the heap memory, when the variable is refrencing to the data in the heap, and another variable also takes the refernece of the same data in the heap, so in that case if the data is changed by any of the two , then the data in the heap also changes resulting in the overall change of the data
+let user = {
+  email: "user@gmail.com",
+  upi: "xyz@Payme",
+};
+
+let userTwo = user;
+userTwo.email = "test@gmail.com";
+
+console.log(user.email);
+console.log(userTwo.email);
